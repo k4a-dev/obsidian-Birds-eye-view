6f8d63331b63b19434e0493cbe21479404f50674
kasahala <kento.iku@gmail.com>
2022-08-05 00:51:02 +0900

    feature: ribbonIconを追加

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import {
+	addIcon,
 	App,
 	Editor,
 	MarkdownView,
@@ -46,6 +47,25 @@ export default class BirdsEyeViewPlugin extends Plugin {
 		});
 
 		this.initLeaf();
+
+		addIcon(
+			"birdseye",
+			`<circle cx="50" cy="50" r="47" stroke="currentColor" stroke-width="6"/>
+			<path d="M5.7988 71.1924L8.34003 93.9546C8.45774 95.0089 9.37811 95.7878 10.4373 95.7297L31.9445 94.5488C34.0875 94.4311 34.5655 91.4795 32.5692 90.6915L19.0917 85.3714C18.6774 85.2078 18.3295 84.9107 18.1033 84.527L9.50918 69.9545C8.41378 68.0971 5.55954 69.0493 5.7988 71.1924Z" fill="currentColor"/>
+			<rect x="22.619" y="22.619" width="15.4762" height="15.4762" rx="1" fill="currentColor"/>
+			<rect x="22.619" y="41.6667" width="15.4762" height="15.4762" rx="1" fill="currentColor"/>
+			<rect x="22.619" y="60.7143" width="15.4762" height="15.4762" rx="1" fill="currentColor"/>
+			<rect x="41.6667" y="22.619" width="15.4762" height="15.4762" rx="1" fill="currentColor"/>
+			<rect x="41.6667" y="41.6667" width="15.4762" height="15.4762" rx="1" fill="currentColor"/>
+			<rect x="41.6667" y="60.7143" width="15.4762" height="15.4762" rx="1" fill="currentColor"/>
+			<rect x="60.7143" y="22.619" width="15.4762" height="15.4762" rx="1" fill="currentColor"/>
+			<rect x="60.7143" y="41.6667" width="15.4762" height="15.4762" rx="1" fill="currentColor"/>
+			<rect x="60.7143" y="60.7143" width="15.4762" height="15.4762" rx="1" fill="currentColor"/>`
+		);
+
+		this.addRibbonIcon("birdseye", "Open bird's eye view", () =>
+			this.initLeaf()
+		);
 	}
 
 	onunload() {
